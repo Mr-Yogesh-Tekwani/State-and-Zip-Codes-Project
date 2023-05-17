a7211c91c0a28817223d1c748554e8438ce424ae
YOGESH TEKWANI <mryogeshtekwani@gmail.com>
2023-05-17 15:18:46 -0400

    Update Combined.swift

--- Milestone 1 (State and Zip)/Combined.swift
+++ Milestone 1 (State and Zip)/Combined.swift
@@ -43,8 +43,42 @@ import Foundation
      }
  }
 
-  
+  1.2 CustomTableCell
+ 
+ class CustomTableViewCell: UITableViewCell {
 
+     static var identifier = "CustomTableViewCell"
+         
+         var stateLabel: UILabel = {
+             let sLabel = UILabel()
+             sLabel.textColor = .systemBlue
+             sLabel.translatesAutoresizingMaskIntoConstraints = false
+             return sLabel
+         }()
+     
+
+         override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+             super.init(style: style, reuseIdentifier: reuseIdentifier)
+             
+             self.addSubview(stateLabel)
+             
+             
+             
+             NSLayoutConstraint.activate(
+                 [stateLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+                  stateLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+                  stateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 10),
+                  stateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+             ])
+         }
+         
+         required init?(coder: NSCoder) {
+             fatalError("init(coder:) has not been implemented")
+         }
+
+ }
+
+ 
  
  2. View
  struct stateZip {
